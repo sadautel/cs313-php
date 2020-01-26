4071d16d8a94402d2c521e93caa6abdd5d701148
Savannah Dautel <sadautel@gmail.com>
2020-01-25 20:24:28 -0700

    fixing error

--- web/week03/week03_b.php
+++ web/week03/week03_b.php
@@ -31,6 +31,83 @@
         $zip = $_POST["zipcode"];
         echo $zip;
     }
+    <br><label>Selected Items:</label>
+$pItems = $_POST['item'];
+
+foreach($pItems as $selected)
+{
+    if($selected == 5.00)
+    { 
+        ?><li><?php
+        echo "Crinkley Plush $";
+        ?><?php echo $selected;?><?php
+        ?></li><?php
+    }
+    if($selected == 5.50)
+    {
+        ?><li><?php
+        echo "Ropes $";
+        ?><?php echo $selected;?><?php
+        ?></li><?php
+    }
+    if($selected == 3.50)
+    {
+        ?><li><?php
+        echo "Balls $";
+        ?><?php echo $selected;?><?php
+        ?></li><?php
+    }
+    if($selected == 6.00)
+    {
+        ?><li><?php
+        echo "Teething Chew $";
+        ?><?php echo $selected;?><?php
+        ?></li><?php
+    }
+    if($selected == 4.00)
+    {
+        ?><li><?php
+        echo "Chewy $";
+        ?><?php echo $selected;?><?php
+        ?></li><?php
+    }
+    if($selected == 4.50)
+    {
+        ?><li><?php
+        echo "Kong $";
+        ?><?php echo $selected;?><?php
+        ?></li><?php
+    }
+    if($selected == 2.50)
+    {
+        ?><li><?php
+        echo "Squeaky Bones $";
+        ?><?php echo $selected;?><?php
+        ?></li><?php
+    }
+    if($selected == 2.00)
+    {
+        ?><li><?php
+        echo "Plushy $";
+        ?><?php echo $selected;?><?php
+        ?></li><?php
+    }
+    if($selected == 3.00)
+    {
+        ?><li><?php
+        echo "Squeaky Plushes $";
+        ?><?php echo $selected;?><?php
+        ?></li><?php
+    }
+}
+
+?>
+<br><label>Purchase total:</label>
+<?php 
+echo " $";
+echo $_POST["total"]
+?>
+<br>
 
 ?>
 </body>
