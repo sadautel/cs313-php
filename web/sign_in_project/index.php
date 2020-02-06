1f13e341dc3870832e2e6d6a0462d3e1b4b94dcf
Savannah Dautel <sadautel@gmail.com>
2020-02-06 16:19:13 -0700

    working on database

--- web/sign_in_project/index.php
+++ web/sign_in_project/index.php
@@ -3,26 +3,25 @@
    $db = get_db();
    $client_name = $db->prepare("SELECT * FROM client");
    $client_name->execute();
+
+   $client_contact = $db->prepare("SELECT * FROM client_contact");
+   $client_contact->execute();
+ 
    while ($nRow = $client_name->fetch(PDO::FETCH_ASSOC))
    {
       $first_name = $nRow["first_name"];
       $last_name = $nRow["last_name"];
 
-      $client_contact = $db->prepare("SELECT * FROM client_contact");
-       $client_contact->execute();
-       while ($cRow = $client_contact->fetch(PDO::FETCH_ASSOC))
-       {
-           $email = $cRow["email"];
-           $phone_number = $cRow["phone_number"];
-           
-       }
-       echo "<p> Name: $first_name $last_name</p>";
-       echo "<p> Contact: $phone_number <br> $email</p>";
-
+      while ($cRow = $client_contact->fetch(PDO::FETCH_ASSOC))
+      {
+          $email = $cRow["email"];
+          $phone_number = $cRow["phone_number"];   
+      }
    }
 
-   
-
   
+
+   echo "<p> Name: $first_name $last_name</p>";
+   echo "<p> Contact: $phone_number <br> $email</p>";
    ?>
 
