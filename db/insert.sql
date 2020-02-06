3a45540c24a347a516374e098b8c0c08c011a63c
Savannah Dautel <sadautel@gmail.com>
2020-02-06 16:08:45 -0700

    working on database

--- db/insert.sql
+++ db/insert.sql
@@ -14,22 +14,6 @@ INSERT INTO client ( first_name
             VALUES ( 'Sue'
                     , 'Dautel');
 
-INSERT INTO client ( first_name
-                    , last_name)
-            VALUES ( 'Sav'
-                    , 'Dautel');
-
-INSERT INTO client ( first_name
-                    , last_name)
-            VALUES ( 'S'
-                    , 'Dautel');
-
-INSERT INTO client ( first_name
-                    , last_name)
-            VALUES ( 'SD'
-                    , 'Dautel');
-
-
 
 INSERT INTO client_contact ( email
                            , phone_number)
@@ -44,23 +28,7 @@ INSERT INTO client_contact ( email
 INSERT INTO client_contact ( email
                            , phone_number)
                     VALUES ( 'example@gmail.com'
-                           , '801-533-9005');
-
-INSERT INTO client_contact ( email
-                           , phone_number)
-                    VALUES ( 'example@gmail.com'
-                           , '801-533-9125');
-
-INSERT INTO client_contact ( email
-                           , phone_number)
-                    VALUES ( 'example@gmail.com'
-                           , '801-533-9555');
-
-INSERT INTO client_contact ( email
-                           , phone_number)
-                    VALUES ( 'example@gmail.com'
-                           , '801-533-9995');
-
+                           , '801-533-9005';
 
 
 INSERT INTO client_check_in ( check_in_date
@@ -77,18 +45,3 @@ INSERT INTO client_check_in ( check_in_date
                             , check_in_time)
                      VALUES ( '02/02/2020'
                             , '1:30 pm');
-
-INSERT INTO client_check_in ( check_in_date
-                            , check_in_time)
-                     VALUES ( '02/02/2020'
-                            , '1:35 pm');
-
-INSERT INTO client_check_in ( check_in_date
-                            , check_in_time)
-                     VALUES ( '02/02/2020'
-                            , '1:40 pm');
-
-INSERT INTO client_check_in ( check_in_date
-                            , check_in_time)
-                     VALUES ( '02/02/2020'
-                            , '1:45 pm');                         
