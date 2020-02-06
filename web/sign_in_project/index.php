9e502cdb59a758291ef2fc6e69f9e7efda9d6bee
Savannah Dautel <sadautel@gmail.com>
2020-02-06 15:58:42 -0700

    working on database

--- web/sign_in_project/index.php
+++ web/sign_in_project/index.php
@@ -8,24 +8,31 @@
       $first_name = $nRow["first_name"];
       $last_name = $nRow["last_name"];
       echo "<p> Name: $first_name $last_name</p>";
+
+      $client_contact = $db->prepare("SELECT * FROM client_contact");
+       $client_contact->execute();
+       while ($cRow = $client_contact->fetch(PDO::FETCH_ASSOC))
+       {
+           $email = $cRow["email"];
+           $phone_number = $cRow["phone_number"];
+            echo "<p> Contact: $phone_number <br> $email</p>";
+
+
+
+            $client_check_in = $db->prepare("SELECT * FROM client_check_in");
+            $client_check_in->execute();
+            while ($iRow = $client_check_in->fetch(PDO::FETCH_ASSOC))
+            {
+               $check_in_date = $iRow["check_in_date "];
+               $check_in_time  = $iRow["check_in_time"];
+               echo "<p> Date and time: $check_in_date $check_in_time</p>";     
+            }
+       }
+
    }
 
-   $client_contact = $db->prepare("SELECT * FROM client_contact");
-   $client_contact->execute();
-   while ($cRow = $client_contact->fetch(PDO::FETCH_ASSOC))
-   {
-      $email = $cRow["email"];
-      $phone_number = $cRow["phone_number"];
-      echo "<p> Contact: $phone_number <br> $email</p>";
-   }
+   
 
-   $client_check_in = $db->prepare("SELECT * FROM client_check_in");
-   $client_check_in->execute();
-   while ($iRow = $client_check_in->fetch(PDO::FETCH_ASSOC))
-   {
-      $check_in_date = $iRow["check_in_date "];
-      $check_in_time  = $iRow["check_in_time"];
-      echo "<p> Date and time: $check_in_date $check_in_time</p>";     
-   }
+  
    ?>
 
