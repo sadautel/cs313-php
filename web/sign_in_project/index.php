1b1c356f0a8264d85737a05ecf4d8f1c9fc23d9f
Savannah Dautel <sadautel@gmail.com>
2020-02-06 16:01:30 -0700

    working on database

--- web/sign_in_project/index.php
+++ web/sign_in_project/index.php
@@ -15,17 +15,18 @@
        {
            $email = $cRow["email"];
            $phone_number = $cRow["phone_number"];
-            echo "<p> Contact: $phone_number <br> $email</p>";
+           
        }
+       echo "<p> Contact: $phone_number <br> $email</p>";
 
        $client_check_in = $db->prepare("SELECT * FROM client_check_in");
        $client_check_in->execute();
        while ($iRow = $client_check_in->fetch(PDO::FETCH_ASSOC))
        {
           $check_in_date = $iRow["check_in_date "];
-          $check_in_time  = $iRow["check_in_time"];
-          echo "<p> Date and time: $check_in_date $check_in_time</p>";     
+          $check_in_time  = $iRow["check_in_time"];    
        }
+       echo "<p> Date and time: $check_in_date $check_in_time</p>"; 
 
    }
 
