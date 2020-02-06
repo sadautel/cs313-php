5017f8746084d1d96c10e1e1dee9336a3af8d2c7
Savannah Dautel <sadautel@gmail.com>
2020-02-06 15:18:11 -0700

    working on database

--- web/sign_in_project/index.php
+++ web/sign_in_project/index.php
@@ -1,14 +1,5 @@
 <?php
-   require "dbConnect.php";
-   $db = get_db();
-   $client_name = $db->prepare("SELECT * FROM client");
-   $client_name->execute();
-   while ($nRow = $client_name->fetch(PDO::FETCH_ASSOC))
-   {
-      $first_name = $nRow["first_name"];
-      $last_name = $nRow["last_name"];
-      echo "<p> Name: $first_name $last_name</p>";
-   }
+   echo "hello";
 
 ?>
 
