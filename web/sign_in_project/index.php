046ead00db8c5cf5adb283c3c63f478e796b7ccc
Savannah Dautel <sadautel@gmail.com>
2020-02-07 11:41:45 -0700

    working on database

--- web/sign_in_project/index.php
+++ web/sign_in_project/index.php
@@ -1,15 +1,12 @@
 <?php
    require "dbConnect.php";
    $db = get_db();
-  foreach ($db->query('SELECT * FROM client') as $row)
+  foreach ($db->query('SELECT * FROM client JOIN client_contact') as $row)
 {
    $first_name = $row["first_name"];
    $last_name = $row["last_name"];
-   foreach ($db->query('SELECT * FROM client_contact') as $cRow)
-   {
-      $email = $cRow["email"];
-      $phone_number = $cRow["phone_number"];
-   } 
+   $email = $cRow["email"];
+   $phone_number = $cRow["phone_number"];
 }
 echo "<p> Name: $first_name $last_name</p>";
 echo "<p> Contact: $phone_number <br> $email</p>";
