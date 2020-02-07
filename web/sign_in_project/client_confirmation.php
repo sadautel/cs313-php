bbb488395603554cfe72d6eeb986161d7bb79fa3
Savannah Dautel <sadautel@gmail.com>
2020-02-07 16:56:31 -0700

    working on database

--- web/sign_in_project/client_confirmation.php
+++ web/sign_in_project/client_confirmation.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
     <title>Document</title>
 </head>
 <body>
@@ -16,8 +17,10 @@
     if($owner = true){
         ?>
         <form action="index.php">
-            First name: <input type="text">
-            Last name: <input type="text">
+            First name: <input type="text"><br>
+            Last name: <input type="text"><br>
+
+            Password: <input type="text"><br>
 
             <input type="submit" name="owner" id="owner" value="Owner" />
         </form>
--- web/sign_in_project/index.php
+++ web/sign_in_project/index.php
@@ -1,3 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+   <link rel="stylesheet" href="style.css">
+   <title>Document</title>
+</head>
+<body>
 <?php
    require "dbConnect.php";
    $db = get_db();
@@ -10,8 +20,10 @@
 
    echo "<p> Client: $first_name  $last_name <br> 
                      $phone_number <br> $email</p>";
-}
-
-   
+}   
    ?>
+</body>
+</html>
+
+
 
