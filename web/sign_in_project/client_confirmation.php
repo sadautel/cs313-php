8a86ce95ff45c5df0f9af46da27ee907f7a7ef6a
Savannah Dautel <sadautel@gmail.com>
2020-02-07 16:01:53 -0700

    working on database

--- web/sign_in_project/client_confirmation.php
+++ web/sign_in_project/client_confirmation.php
@@ -12,13 +12,23 @@
     $confirm =  $_POST["confirm"];
     $confirm = false;
 
+    $owner =  $_POST["owner"];
+    $owner = false;
+
     if($confirm = true){
         ?>
         <P> Thank you for signing in!</p>
         <?php
     }
+    elseif($owner = true){
+        ?>
+        <form action="index.php">
+            <input type="text">
+        </form>
+        <?php
+    }
     else{
-        
+
     }
 
     ?>
