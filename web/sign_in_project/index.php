3eb0f29b6b13c44bd80ab161e89dd593ea02f1e1
Savannah Dautel <sadautel@gmail.com>
2020-02-07 11:38:16 -0700

    working on database

--- web/sign_in_project/index.php
+++ web/sign_in_project/index.php
@@ -5,16 +5,15 @@
 {
    $first_name = $row["first_name"];
    $last_name = $row["last_name"];
+   echo "<p> Name: $first_name $last_name</p>";
 
    foreach ($db->query('SELECT * FROM client_contact') as $cRow)
    {
       $email = $cRow["email"];
       $phone_number = $cRow["phone_number"];
    
-      
-   }
-   echo "<p> Name: $first_name $last_name</p>";
-   echo "<p> Contact: $phone_number <br> $email</p>";
+      echo "<p> Contact: $phone_number <br> $email</p>";
+   } 
 }
    
    ?>
