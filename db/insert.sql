980f546be3fb4cb2c94f1a70fc8edec11514e96a
Savannah Dautel <sadautel@gmail.com>
2020-02-07 12:14:18 -0700

    working on database

--- db/insert.sql
+++ db/insert.sql
@@ -26,21 +26,3 @@ INSERT INTO client_check_in ( first_name
                             , 'example@gmail.com'
                             , '801-530-9999');
 
-INSERT INTO client_check_in ( first_name
-                            , last_name
-                            , email
-                            , phone_number)
-                     VALUES ( 'S'
-                            , 'Dautel'
-                            , 'example@gmail.com'
-                            , '801-530-5555');
-
-INSERT INTO client_check_in ( first_name
-                            , last_name
-                            , email
-                            , phone_number)
-                     VALUES ( 'Sue'
-                            , 'Dautel'
-                            , 'example@gmail.com'
-                            , '801-530-0000');
-
