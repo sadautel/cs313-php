7ee8893bdc4f9efb79f0b183f8cc1e423ff85fec
Savannah Dautel <sadautel@gmail.com>
2020-02-08 09:10:36 -0700

    working on database

--- web/sign_in_project/search_result.php
+++ web/sign_in_project/search_result.php
@@ -1,15 +1,21 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
 
+<?php
 require "dbConnect.php";
 $db = get_db();
 $valueToSearch = $_POST['valueToSearch'];
 
 $db = "SELECT * FROM user WHERE f_name=$valueToSearch ";
 $result = $db;
-
-?>
-
-<?php
 
 if ($result)
  {
@@ -23,9 +29,9 @@ if ($result)
 <td><?php echo $row["f_name"]; ?></td>
 <td><?php echo $row["l_name"]; ?></td>
 </tr>
-
-<?php
- }
-} 
-?>
 </table>
+    
+</body>
+</html>
+
+
