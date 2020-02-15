9663344e22f9054d78a9e14583e79375dbe03ec3
Savannah Dautel <sadautel@gmail.com>
2020-02-15 13:50:52 -0700

    working of inserting into db

--- web/sign_in_project/client_sign_in.php
+++ web/sign_in_project/client_sign_in.php
@@ -26,7 +26,6 @@
         Phone:  <input type="text" name="phone"><br>
         </div>
         <br><br>
-                  <select name="client_input">
                      <?php
                         $statement = $db->prepare("SELECT * FROM clinet_info");
                         $statement->execute();
@@ -37,9 +36,10 @@
                            $last_name = $row["last_name"];
                            $email = $row["email"];
                            $phone_number = $row["phone_number"];
+
+                    
                         }
                      ?>
-                  </select>
         <input type="submit" name="confirm" id="cfm" value="Sign in" />
         <input type="submit" name="owner" id="owner" value="Owner" />
       </form>
