2f8e19d80816074fdbad29d5d8a100c685759267
Savannah Dautel <sadautel@gmail.com>
2020-02-20 16:08:59 -0700

    trying something

--- web/sign_in_project/search.php
+++ web/sign_in_project/search.php
@@ -21,8 +21,12 @@ while ($row = $search->fetch(PDO::FETCH_ASSOC))
     $id = $row['id'];
     $first_name = $row["first_name"];
     $last_name = $row["last_name"];
+    $email = $row["email"];
+    $phone_number = $row["phone_number"];
 
-   echo "Client: $first_name $last_name";
+ echo "<p> Client: $first_name  $last_name <br> 
+                   $phone_number <br> $email</p>";
+
 }
 
 
