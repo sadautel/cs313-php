9a6bbfc2f788a589ae875d89b6cc2867a33ef66d
Savannah Dautel <sadautel@gmail.com>
2020-02-20 15:39:09 -0700

    trying something

--- web/sign_in_project/index.php
+++ web/sign_in_project/index.php
@@ -28,7 +28,7 @@
    require "dbConnect.php";
    $db = get_db();
    $personId = $_GET['personId'];
-   $statement = $db->prepare('SELECT * FROM client_info WHERE Id = :personId');
+   $statement = $db->prepare('SELECT * FROM client_info');
    $statement->bindValue(':personId', $personId);
    $statement->execute();
    while ($row = $statement->fetch(PDO::FETCH_ASSOC))
