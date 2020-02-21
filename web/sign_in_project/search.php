821385a72e282d98824b3268526e67d51a47e652
Savannah Dautel <sadautel@gmail.com>
2020-02-21 14:41:02 -0700

    Brother Birch hacking

--- web/sign_in_project/search.php
+++ web/sign_in_project/search.php
@@ -32,24 +32,13 @@ echo $valueToSearch;
 
 // }
 
-$personId = 1;
-$statement = $db->prepare('SELECT * FROM w6_user WHERE Id = :personId');
-$statement->bindValue(':personId', $personId);
+$statement = $db->prepare('SELECT * FROM client_info WHERE first_name = :personId');
+$statement->bindValue(':personId', $valueToSearch);
 $statement->execute();
 while ($row = $statement->fetch(PDO::FETCH_ASSOC))
 {
    $id      = $row['id'];
-   $first   = $row['first_name'];
-   $last    = $row['last_name'];
-   $food_id = $row['food_type'];
-
-   $foods = $db->prepare("SELECT food FROM w6_food WHERE ID = $food_id");
-   $foods->execute();
-   while ($fRow = $foods->fetch(PDO::FETCH_ASSOC))
-   {
-      $food = $fRow['food'];
-   }
-   echo "<h1>$first $last's favorite food is $food</h1>";
+   echo "$id";
 }
 
 
