3a9cd2310440f18e9c99f3c06be1f7db9430ac70
Savannah Dautel <sadautel@gmail.com>
2020-02-21 14:31:12 -0700

    Brother Birch hacking

--- web/sign_in_project/search.php
+++ web/sign_in_project/search.php
@@ -15,9 +15,9 @@ require "dbConnect.php";
 $db = get_db();
 $valueToSearch = $_POST['valueToSearch'];
 echo $valueToSearch;
-// $search = $db->prepare("SELECT * FROM client_info WHERE first_name = :value");
-// $statement->bindValue(':value', $valueToSearch);
-// $search->execute();
+$search = $db->prepare("SELECT * FROM client_info WHERE first_name = :value");
+$statement->bindValue(':value', $valueToSearch);
+$search->execute();
 echo "SELECT * FROM client_info WHERE first_name = \'$valueToSearch\'";
 // while ($row = $search->fetch(PDO::FETCH_ASSOC))
 // {
