fb82e6bfc0c1c4b2267646e34d6b1c44ceaa1e51
Savannah Dautel <sadautel@gmail.com>
2020-02-21 14:10:34 -0700

    trying something

--- web/sign_in_project/client_confirmation.php
+++ web/sign_in_project/client_confirmation.php
@@ -33,7 +33,7 @@ catch (Exception $ex)
 	echo "Error with DB. Details: $ex";
 	die();
 }
-header("Location: index.php/?personId=$userId");
+header("Location: index.php?personId=$userId");
 
 die();
 ?>
