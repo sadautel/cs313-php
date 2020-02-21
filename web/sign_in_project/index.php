8d5a577c1b659b6e4bf673c6895bed3e562f431f
Savannah Dautel <sadautel@gmail.com>
2020-02-21 14:14:23 -0700

    Brother Birch hacking

--- web/sign_in_project/index.php
+++ web/sign_in_project/index.php
@@ -16,7 +16,7 @@
 
 <header>
 <br>
-<form action="search.php" method="post" >
+<form action="search.php" method="POST" >
 <input type="text" name="valueToSearch" placeholder="Search Record.."></br>
 <input type="submit" name="search" value="Search Record..">
 </form>
