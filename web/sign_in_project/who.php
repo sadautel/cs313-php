15d4e76b57a9c6df9e5384970f57cafc8a2ba121
Savannah Dautel <sadautel@gmail.com>
2020-02-22 19:47:54 -0700

    working

--- web/sign_in_project/who.php
+++ web/sign_in_project/who.php
@@ -24,7 +24,7 @@
                   $_POST['password'] == '1234') {
                   $_SESSION['valid'] = true;
                   $_SESSION['timeout'] = time();
-                  $_SESSION['username'] = 'tutorialspoint';
+                  $_SESSION['username'] = 'project';
                   
                   echo 'You have entered valid use name and password';
                }else {
