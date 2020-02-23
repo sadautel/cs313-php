b8013127fede45ec4c2e78b8bd0c57742c4dacc4
Savannah Dautel <sadautel@gmail.com>
2020-02-22 19:35:23 -0700

    working

--- web/sign_in_project/who.php
+++ web/sign_in_project/who.php
@@ -9,12 +9,42 @@
 <body>
 <header>
         <h1>
-            Please Select One
+            Please login
         </h1>
     </header>
-<form action="client_sign_in.php" method="POST">
-       <input type="submit" name="owner" value="owner" checked> Owner<br>
-       <input type="submit" name="client" value="client"> Client<br>
-      </form>
+
+
+    <?php
+            $msg = '';
+            
+            if (isset($_POST['login']) && !empty($_POST['username']) 
+               && !empty($_POST['password'])) {
+				
+               if ($_POST['username'] == 'tutorialspoint' && 
+                  $_POST['password'] == '1234') {
+                  $_SESSION['valid'] = true;
+                  $_SESSION['timeout'] = time();
+                  $_SESSION['username'] = 'tutorialspoint';
+                  
+                  echo 'You have entered valid use name and password';
+               }else {
+                  $msg = 'Wrong username or password';
+               }
+            }
+         ?>
+      
+      
+      <form action="client_sign_in.php" method="POST">
+            action = "<?php echo htmlspecialchars($_SERVER['PHP_SELF']); 
+            ?>" method = "post">
+            <h4 class = "form-signin-heading"><?php echo $msg; ?></h4>
+            <input type = "text" class = "form-control" 
+               name = "username" placeholder = "username = tutorialspoint" 
+               required autofocus></br>
+            <input type = "password" class = "form-control"
+               name = "password" placeholder = "password = 1234" required>
+            <button class = "btn btn-lg btn-primary btn-block" type = "submit" 
+               name = "login">Login</button>
+         </form>
 </body>
 </html>
